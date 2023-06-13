ff7bedc6a76f2efdfb79013f3bafbf1cc196117d
Eugene_Pro <proskuryakov.e@list.ru>
2023-06-13 12:56:51 +0300

    a model class has been created

--- toyRaffleInStore/ParticipantModel.java
+++ toyRaffleInStore/ParticipantModel.java
@@ -1,6 +1,38 @@
 package toyRaffleInStore;
 
 public class ParticipantModel {
+    private String id;
+    private String name;
+    private int frequency;
 
+    public ParticipantModel(String id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
 
 }
